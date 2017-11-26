6d98daf7e0c83aea43c216bdef7194c1ab31f098
Sergey Abramyan <sa.abramyan@gmail.com>
2017-11-26 17:23:19 +0300

    Add redis support

--- src/proximity_service.erl
+++ src/proximity_service.erl
@@ -3,9 +3,17 @@
 -export([start/0, start/1]).
 -export([get_env/1]).
 -export([publish_event/3]).
+-export([publish_event_ex/4]).
+-export([get_ex_data/1]).
+-export([del_ex_data/1]).
 
 -define(SQS_POOL_NAME, proximity_service_sqs_pool).
 -define(SQS_POOL_SIZE, 10).
+-define(REDIS_HOST, "127.0.0.1").
+-define(REDIS_PORT, 6379).
+-define(REDIS_POOL_NAME, proximity_service_redis_pool).
+-define(REDIS_POOL_SIZE, 15).
+-define(REDIS_EXPIRE, 3 * 60 * 60).
 
 %%====================================================================
 %% API
@@ -21,6 +29,7 @@ start() ->
 
 start({_M, _F} = Handler) ->
 	ok = start_sqs(Handler),
+	ok = start_redis(),
 	ok.
 
 get_env(Key) ->
@@ -29,28 +38,57 @@ get_env(Key) ->
 get_env(Key, Default) ->
 	application:get_env(?MODULE, Key, Default).
 
-publish_event(Topic, Event, Payload) when is_atom(Topic), is_binary(Event), is_map(Payload); is_binary(Payload) ->
-	case topic_arn_by_topic(Topic) of
-		{ok, TopicArn} ->
-			EventData = event_data(Event, Payload),
-			_ = erlcloud_sns:publish_to_topic(TopicArn, jiffy:encode(EventData), undefined),
-			ok;
-		error ->
-			lager:error("Topic ~p not found", [Topic]),
-			{error, not_found}
-	end.
+publish_event(Topic, Event, Payload) when is_atom(Topic), is_binary(Event), is_map(Payload) ->
+	EventData = event_data(Event, Payload),
+	publish_to_topic(Topic, EventData).
+
+publish_event_ex(Topic, Event, Payload, ExData) when is_atom(Topic), is_binary(Event), is_map(Payload), is_binary(ExData) ->
+	ExDataId = gen_id(),
+	ok = set_ex_data(ExDataId, ExData),
+	EventData = event_data(Event, Payload, #{<<"ex_data_id">> => ExDataId}),
+	publish_to_topic(Topic, EventData).
 
 topic_arn_by_topic(Topic) ->
 	Topics = get_env(topics, #{}),
 	maps:find(Topic, Topics).
 
+get_ex_data(#{<<"system">> := #{<<"ex_data_id">> := ExDataId}}) ->
+    case redis_q(["GET", ExDataId]) of
+        {ok, undefined} ->
+            {error, not_found};
+        {error, query_fail} ->
+            {error, not_found};
+        {ok, Value} ->
+            {ok, Value}
+    end.
+
+del_ex_data(#{<<"system">> := #{<<"ex_data_id">> := ExDataId}}) ->
+	case redis_q(["DEL", ExDataId]) of
+        {ok, _} ->
+            ok;
+        {error, query_fail} ->
+            ok
+    end.
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
+publish_to_topic(Topic, EventData) ->
+	case topic_arn_by_topic(Topic) of
+		{ok, TopicArn} ->
+			_ = erlcloud_sns:publish_to_topic(TopicArn, jiffy:encode(EventData), undefined),
+			ok;
+		error ->
+			{error, not_found}
+	end.
+
 event_data(Event, Payload) ->
-	System = #{<<"timestamp">> => unix_timestamp(), <<"publisher">> => atom_to_binary(node(), utf8)},
-	#{<<"event">> => Event, <<"payload">> => Payload, <<"system">> => System}.
+	event_data(Event, Payload, #{}).
+
+event_data(Event, Payload, System) ->
+	NewSystem = System#{<<"timestamp">> => unix_timestamp(), <<"publisher">> => atom_to_binary(node(), utf8)},
+	#{<<"event">> => Event, <<"payload">> => Payload, <<"system">> => NewSystem}.
 
 unix_timestamp() ->
     DateTime = calendar:universal_time(),
@@ -61,3 +99,38 @@ start_sqs(Handler) ->
     ChildMF = {proximity_service_sqs, start_link},
     _ = cuesport:start_link(?SQS_POOL_NAME, ?SQS_POOL_SIZE, ChildMods, ChildMF, {for_all, [Handler]}),
     ok.
+
+start_redis() ->
+	ChildMods = [eredis],
+    ChildMF = {eredis, start_link},
+    RedisHost = get_env(redis_host, ?REDIS_HOST),
+    _ = cuesport:start_link(?REDIS_POOL_NAME, ?REDIS_POOL_SIZE, ChildMods, ChildMF, {for_all, [RedisHost, ?REDIS_PORT]}),
+    ok.
+
+set_ex_data(ExDataId, ExData) ->
+	case redis_q(["SETEX", ExDataId, ?REDIS_EXPIRE, ExData]) of
+        {ok, <<"OK">>} ->
+            ok;
+        {error, query_fail} ->
+            ok
+    end.
+
+gen_id() ->
+	list_to_binary(uuid:uuid_to_string(uuid:get_v4())).
+
+redis_q(Query) ->
+	try eredis:q(get_redis_worker(), Query) of
+        {error, _Reason} = Error ->
+            lager:warning("Failed run redis query ~p ~p", [Query, Error]),
+            {error, query_fail};
+        Ret ->
+            Ret
+    catch
+        C:R ->
+            ST = erlang:get_stacktrace(),
+            lager:warning("Failed run redis query ~p ~p ~p", [Query, {C, R}, ST]),
+            {error, query_fail}
+    end.
+
+get_redis_worker() ->
+    cuesport:get_worker(?REDIS_POOL_NAME).
